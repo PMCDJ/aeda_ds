198ee5ed600929edf33c6b6195d8c4cc63486889
PMCDJ <58326660+PMCDJ@users.noreply.github.com>
2020-05-06 11:35:30 +0100

    Add files via upload

---
+++ aeda_ds/aeda_ds/exceptions.py
@@ -0,0 +1,9 @@
+class EmptyListException(Exception):
+    print("Não existe elementos na lista.")
+
+class InvalidPositionException(Exception):
+    print("Posição invalida.")
+
+class NoSuchElementException(Exception):
+    print("Não existe o elemento pedido.")
+    
