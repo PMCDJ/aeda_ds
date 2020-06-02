9f5fca9c1da7de74ae69c10390d8883ef1bfa1c7
PMCDJ <pedrojoaq73@gmail.com>
2020-06-02 16:20:01 +0000

    alteraçoes

--- aed_ds/dictionaries/hash_table.py
+++ aed_ds/dictionaries/hash_table.py
@@ -4,16 +4,27 @@ from ..lists.singly_linked_list import SinglyLinkedList
 
 class HashTable(Dictionary):
     def __init__(self, size=101):
-        pass
+        self.array_size = size
+        self.num_elements = 0
+        self.table = (self.array_size * ctypes.py_object)()
+        for i in range (self.array_size):
+            self.table[i] = SinglyLinkedList()
+    
+    def hash_function(self,k):
+        return sum([ord(c) for  c in k]) % self.array_size
 
-    def size(self): pass
+    def size(self):
+        return self.array_size
 
-    def is_full(self): pass
+    def is_full(self):
+        if self.array_size == 0:
+            return True
+        return False
 
-    def get(self, k): pass
 
-    def insert(self, k, v): pass
-
+    def insert(self, k, v): 
+        if is_full == False:
+            
     def update(self, k, v): pass
 
     def remove(self, k): pass
