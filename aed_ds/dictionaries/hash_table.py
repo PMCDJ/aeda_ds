dd30053088787232195cf877fa6988113e747391
PMCDJ <pedrojoaq73@gmail.com>
2020-06-02 16:51:55 +0000

    hash table error

--- aed_ds/dictionaries/hash_table.py
+++ aed_ds/dictionaries/hash_table.py
@@ -1,36 +1,120 @@
 from .tad_dictionary import Dictionary
-from ..exceptions import NoSuchElementException, DuplicatedKeyException
+from ..exceptions import NoSuchElementException, DuplicatedKeyException, InvalidPositionException, EmptyListException
 from ..lists.singly_linked_list import SinglyLinkedList
+from .item import Item
 
+import ctypes
 class HashTable(Dictionary):
-    def __init__(self, size=101):
-        self.array_size = size
+    def __init__(self, size = 101):
         self.num_elements = 0
+        self.array_size = size
         self.table = (self.array_size * ctypes.py_object)()
-        for i in range (self.array_size):
+
+        for i in range(self.array_size):
             self.table[i] = SinglyLinkedList()
-    
-    def hash_function(self,k):
-        return sum([ord(c) for  c in k]) % self.array_size
 
     def size(self):
-        return self.array_size
+        return self.num_elements
 
     def is_full(self):
-        if self.array_size == 0:
-            return True
+        #dispersão fechada
+        if self.num_elements == self.array_size:
+            return True 
+        #dispersão aberta
         return False
 
+    def get(self, k):
+        #if self.has_key(k):
+            #raise NoSuchElementException() 
+        i = self.hash_function(k)
+        colision_list = self.table[i]
+        it = colision_list.iterator()
+        while it.has_next():
+            item = it.next()
+            if item.get_key == k:
+                return item.get_value()
+        raise NoSuchElementException()
 
     def insert(self, k, v): 
-        if is_full == False:
+        if self.has_key(k):
+            raise DuplicatedKeyException()
+        idx = self.hash_function(k)
+        item = Item(k,v)
+        self.table[idx].insert_first(item)
+        self.num_elements += 1 
+        
+    def update(self, k, v): 
+        if not self.has_key(k):
+            raise NoSuchElementException()
+        idx = self.hash_function(k)
+        colition_list = self.table[idx]
+        it = colition_list.iterator()
+        while it.has_next():
+            current_item = it.next()
+            if current_item.get_key() == k:
+                return self.current_item.set_value(v)
+        
             
-    def update(self, k, v): pass
+    def remove(self, k):
+        if not self.has_key(k):
+            raise NoSuchElementException()
+        idx = self.hash_function(k)
+        it = self.table[idx].iterator()
+        pos = -1
+        while it.has_next():
+            item = it.next()
+            pos += 1
+            if item.get_key() == k:
+                # Found it
+                break
+        # pos is the position to remove from the colision list
+        self.table[idx].remove(pos)
+        self.num_elements -= 1
+        
 
-    def remove(self, k): pass
+    def keys(self):
+        if self.num_elements == 0 :
+            raise NoSuchElementException()
+        for idx in range (self.array_size):
+            colition_list = self.table[idx]
+            it = colition_list.iterator()
+            while it.has_next():
+                current_item = it.next()
+                if current_item.get_key():
+                    return self.current_item.get_keys()
+        
+    def values(self): 
+        if self.num_elements == 0 :
+            raise NoSuchElementException()
+        for idx in range (self.array_size):
+            colition_list = self.table[idx]
+            it = colition_list.iterator()
+            while it.has_next():
+                item = Item(k,v)
+                item = it.next()
+                if item.get_key():
+                    return self.item.get_value()
+        
+    def items(self): 
+        if self.num_elements == 0:
+            raise NoSuchElementException()
+        for idx in range(self.array_size):
+            colition_list = self.table[idx]
+            it = colition_list.iterator()
+            while it.has_next:
+                curent_item = it.next()
+                if curent_item:
+                    print(self.curent_item)
 
-    def keys(self): pass
-
-    def values(self): pass
-
-    def items(self): pass
+    def hash_function(self, k):
+        return sum([ord(c) for c in k]) % self.array_size
+    
+    def has_key(self, k):
+        idx = self.hash_function(k) # O(1)    
+        colision_list = self.table[idx]
+        it = colision_list.iterator()
+        while it.has_next():
+            current_item = it.next()
+            if current_item.get_key() == k:
+                return True
+        return False
